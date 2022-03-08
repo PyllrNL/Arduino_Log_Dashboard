e34771ff5aafb463465712d82a891d965dcafae5
Folkert Kevelam <folkert@pyllr.nl>
2022-03-08 18:27:25 +0100

    Change to key based system

--- src/cli.py
+++ src/cli.py
@@ -1,13 +1,25 @@
-import db
+import secrets
 import sys
 import argparse
+import datetime
+import sqlite3
+import string
 
 parser = argparse.ArgumentParser(description='Client for database')
-parser.add_argument('--name', nargs='?', help='name of new device')
+parser.add_argument('--key', help='generate new login key', action="store_true")
 
 args = parser.parse_args()
 
-if args.name:
-    database = db.database(db.database_file)
-    key = database.new_device(args.name)
-    print(key[0])
+if args.key:
+    db = sqlite3.connect("dashboard.db")
+    cur = db.cursor()
+    alphabet = string.ascii_uppercase + string.digits
+    secret_key = ''.join(secrets.choice(alphabet) for i in range(5))
+    end_date = datetime.datetime.now() + datetime.timedelta(hours=24)
+    timestamp = datetime.datetime.timestamp(end_date)
+    cur.execute("INSERT INTO login_keys (TIMESTAMP, KEY) VALUES(:time, :key)",
+            {"time":int(timestamp), "key":secret_key})
+    db.commit()
+    db.close()
+    print(secret_key)
+
