04bcffebc7430fddb9820042e8956673ab204209
Folkert Kevelam <folkert@pyllr.nl>
2022-03-10 15:31:00 +0100

    Add demo code

--- src/test.py
+++ src/test.py
@@ -7,51 +7,34 @@ import string
 
 import base64
 import msgpack
-i = 1
+i = 0
 
-def construct_data(samples, sample_freq, v_peak, c_peak):
+def construct_data(samples):
     global i
     current = list()
     voltage = list()
-
     for j in range(0, samples):
-        noise = random.randint(-100, 100) / 2.5
-        temp_v = (v_peak * math.sin(2*math.pi*(25.0 / sample_freq) * i)) + noise
-        temp_c =  c_peak * math.sin(2*math.pi*(25.0 / sample_freq)*i + (0.5 * math.pi))
-        temp_c += (c_peak/2) * math.sin(2*math.pi*(123.0 / sample_freq)*i + (0.5 * math.pi))
-        voltage.append(int(temp_v))
+        temp_v = 5.0 * math.sin(2*math.pi*141*( i / 4000.0))
+        temp_v += math.sin(2*math.pi*300*( i / 4000.0))
+        #temp_v += (random.randint(-100, 100) / 250.0)
+        temp_c = 0.01 * math.sin(2*math.pi*57*((i) / 4000.0) + (0.5 * math.pi))
+        temp_c += 0.005 * math.sin(2*math.pi*100*((i) / 4000.0))
+        voltage.append(temp_v)
         current.append(temp_c)
         i = i + 1
-    return (voltage, current)
-
-def get_rms_values(samples):
-    voltage = list()
-    current = list()
-    for i in range(0, samples):
-        v_peak = random.randint(320, 322)
-        c_peak = random.randint(10, 10) / 10.0
-        v, c = construct_data(101, v_peak, c_peak)
-        rms_voltage = 0
-        rms_current = 0
-        for i in range(0, len(v)):
-            rms_voltage += v[i] * v[i]
-            rms_current += c[i] * c[i]
-        rms_voltage = rms_voltage / len(v)
-        rms_current = rms_current / len(c)
-        rms_voltage = math.sqrt(rms_voltage)
-        rms_current = math.sqrt(rms_current)
-        voltage.append(rms_voltage)
-        current.append(rms_current)
     return (voltage, current)
 
 
 async def hello():
     random.seed(time.time())
-    async with websockets.connect("ws://167.71.68.242:80/device/03pwbIIosvgLQhC_USUHmN6Iz-86xyY_X7noo-jksMc") as websocket:
+    async with websockets.connect("ws://localhost:8080/device/03pwbIIosvgLQhC_USUHmN6Iz-86xyY_X7noo-jksMc") as websocket:
         response = await websocket.recv()
         while True:
-            raw = construct_data(100, 777, 322, 1.0)
-            packed = msgpack.packb([raw[0], raw[1]], use_bin_type=True)
+            raw = construct_data(100)
+            data = dict()
+            data["0"] = raw[0]
+            data["1"] = raw[1]
+            packed = msgpack.packb(data, use_bin_type=True)
             encoded = base64.b64encode(packed)
             await websocket.send(encoded)
             await asyncio.sleep(1)
