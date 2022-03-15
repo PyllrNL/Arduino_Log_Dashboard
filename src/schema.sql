145a80c3f5e77a8cad92d2d92252701320698584
Folkert Kevelam <folkert@pyllr.nl>
2022-03-15 11:16:18 +0100

    Update schema for new resthandler

--- src/schema.sql
+++ src/schema.sql
@@ -1,43 +1,35 @@
-create table login_keys (
-	id			INTEGER PRIMARY KEY AUTOINCREMENT,
-	timestamp		INTEGER NOT NULL,
-	key			TEXT NOT NULL
+create table users (
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    username            TEXT NOT NULL,
+    hashed_password     TEXT NOT NULL,
 );
 
-create table users (
-	id			INTEGER PRIMARY KEY AUTOINCREMENT,
-	username		text NOT NULL,
-	hashed_password 	text NOT NULL
+create table login_keys (
+	id			        INTEGER PRIMARY KEY AUTOINCREMENT,
+	timestamp		    INTEGER NOT NULL,
+	key			        TEXT NOT NULL
 );
 
 create table api_keys (
-	id			INTEGER PRIMARY KEY AUTOINCREMENT,
-	user_id			INTEGER NOT NULL,
-	key			text NOT NULL,
+	id			        INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_id			    INTEGER NOT NULL,
+	key			        text NOT NULL,
 	FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 create table devices (
-	id			INTEGER PRIMARY KEY AUTOINCREMENT,
-	user_id			INTEGER NOT NULL,
-	name			text NOT NULL,
-	device_key		text NOT NULL,
+	id			        INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_id			    INTEGER NOT NULL,
+	name			    TEXT NOT NULL,
+	device_key		    TEXT NOT NULL,
+    structure           BLOB NOT NULL,
 	FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-create table device_fields (
-	id			INTEGER PRIMARY KEY AUTOINCREMENT,
-	device_id		INTEGER NOT NULL,
-	field_id		INTEGER NOT NULL,
-	field			text NOT NULL,
-	FOREIGN KEY(device_id) REFERENCES devices(id)
-);
-
 create table samples (
-	id			INTEGER PRIMARY KEY AUTOINCREMENT,
-	device_id		INTEGER NOT NULL,
-	timestamp		INTEGER NOT NULL,
-	voltage			REAL NOT NULL,
-	current			REAL NOT NULL,
+	id			        INTEGER PRIMARY KEY AUTOINCREMENT,
+	device_id		    INTEGER NOT NULL,
+	timestamp		    INTEGER NOT NULL,
+	data                BLOB NOT NULL,
 	FOREIGN KEY(device_id) REFERENCES devices(id)
 );
