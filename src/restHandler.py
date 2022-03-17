9e2cc23c70a1888cc503337fbc50f362e9d30f5d
Folkert Kevelam <folkert@pyllr.nl>
2022-03-17 17:25:08 +0100

    Finish the rest api handler

--- src/restHandler.py
+++ src/restHandler.py
@@ -4,16 +4,16 @@ import tornado.web
 import base
 import json
 import secrets
+import msgpack
 
 class RestBaseHandler(base.BaseHandler):
     def authenticate(self):
-        return 1
-        #user_key = self.get_secure_cookie(self.application.cookie_name)
-        #if user_key == None:
-        #    raise tornado.web.HTTPError(403)
-        #if user_key not in self.application.user_sessions:
-        #    self.redirect("/auth/login")
-        #return self.application.user_sessions[user_key]
+        user_key = self.get_secure_cookie(self.application.cookie_name).decode("utf-8")
+        if user_key == None:
+            raise tornado.web.HTTPError(403)
+        if user_key not in self.application.user_sessions:
+            self.redirect("/auth/login")
+        return self.application.user_sessions[user_key]
 
     def parse_int_argument( self, name, default, minimum, maximum ):
         arg = self.get_argument(name, str(default))
@@ -123,7 +123,6 @@ class RestBaseHandler(base.BaseHandler):
         response = dict()
 
         statement = "SELECT * FROM devices WHERE device_key=:device_key"
-        print(device_id)
         result = await self.query(statement, {"device_key" : device_id})
 
         response["id"] = result[0].device_key
@@ -148,7 +147,6 @@ class RestBaseHandler(base.BaseHandler):
         response = dict()
 
         statement = "SELECT * FROM devices WHERE name=:name"
-        print(device_name)
         result = await self.query(statement, {"name" : device_name})
 
         response["id"] = result[0].device_key
@@ -169,14 +167,240 @@ class RestBaseHandler(base.BaseHandler):
 
         return response
 
-    async def update_device( self, user_id, structure ):
-        pass
-
     async def delete_device_with_id( self, user_id, device_id ):
-        pass
+        response = dict()
+
+        statement = "SELECT * FROM devices WHERE device_key=:device_key"
+        result = await self.query(statement, {"device_key" : device_id})
+
+        response["id"] = result[0].device_key
+        response["name"] = result[0].name
+
+        statement = "SELECT * FROM device_fields WHERE device_id=:device_id\
+                ORDER BY field_index ASC"
+        result = await self.query(statement, {
+                "device_id" : result[0].id
+            })
+
+        response["fields"] = list()
+        for I in result:
+            response["fields"].append({
+                    "type" : I.field_type,
+                    "name" : I.field_name
+                })
+
+        statement = "DELETE FROM device_fields WHERE device_id=:device_id"
+        await self.query(statement, {"device_id" : result[0].id })
+
+        statement = "DELETE FROM devices WHERE device_key=:device_key"
+        await self.query(statement, {"device_key" : device_id})
+        await self.application.db.commit()
+
+        return response
 
     async def delete_device_with_name( self, user_id, device_name ):
-        pass
+        response = dict()
+
+        statement = "SELECT * FROM devices WHERE name=:name"
+        result = await self.query(statement, {"name" : device_name})
+
+        response["id"] = result[0].device_key
+        response["name"] = result[0].name
+
+        device_id = result[0].id
+
+        statement = "SELECT * FROM device_fields WHERE device_id=:device_id\
+                ORDER BY field_index ASC"
+        result = await self.query(statement, {
+                "device_id" : result[0].id
+            })
+
+        response["fields"] = list()
+        for I in result:
+            response["fields"].append({
+                    "type" : I.field_type,
+                    "name" : I.field_name
+                })
+
+        statement = "DELETE FROM device_fields WHERE device_id=:device_id"
+        await self.query(statement, {"device_id" : device_id })
+
+        statement = "DELETE FROM devices WHERE device_key=:device_key"
+        await self.query(statement, {"device_key" : response["id"]})
+        await self.application.db.commit()
+
+        return response
+
+    async def get_samples( self, device_id, limit, page, ascending ):
+        response = dict()
+
+        statement = "SELECT * from devices where device_key=:device_key"
+        result = await self.query(statement, {"device_key": device_id})
+        device = result[0]
+
+        print(device_id)
+        statement = "SELECT COUNT(*) from samples where device_id=:device_id"
+        result = await self.querycount(statement, {"device_id": device.id})
+
+        div = int(result / limit)
+        response["total_pages"] = div if div > 0 else 1
+        response["page"] = page
+        response["data"] = dict()
+
+        if result == 0:
+            return response
+
+        retrieve_limit = limit * (page + 1)
+
+        statement = "SELECT * from samples where device_id=:device_id ORDER BY\
+                id {} LIMIT {}".format(
+                        "ASC" if ascending else "DESC",
+                        retrieve_limit)
+        
+        result = await self.query(statement, {
+                "device_id" : device.id,
+            })
+
+        statement = "SELECT * from device_fields where device_id=:device_id"
+        device_fields = await self.query(statement, {"device_id" : device.id})
+
+        response["data"]["timestamp"] = list()
+        value_index = list()
+        for I in device_fields:
+            response["data"][I.field_name] = list()
+            value_index.insert(I.field_index, I.field_name)
+
+        for I in range(limit * page, len(result)):
+            response["data"]["timestamp"].append(result[I].timestamp)
+            data = msgpack.unpackb( result[I].data )
+            for J in range(0, len(data)):
+                response["data"][value_index[J]].append(data[J])
+
+        return response
+
+    async def get_dashboards_with_user(self, user_id):
+        response = list()
+
+        statement = "SELECT * FROM dashboards WHERE user_id=:user_id"
+        result = await self.query(statement, {"user_id": user_id})
+
+        for I in result:
+            single_result = dict()
+            single_result["id"] = I.id
+            single_result["samples"] = I.samples
+            single_result["fields"] = list()
+
+            statement = "SELECT * FROM dashboard_fields WHERE dashboard_id\
+                    =:dashboard_id"
+            fields = await self.query( statement, {"dashboard_id" : I.id })
+            for J in fields:
+                statement = "SELECT * FROM device_fields WHERE id=:id"
+                dev_fields = await self.query( statement, {"id" : J.device_fields_id})
+                dev_fields = dev_fields[0]
+                statement = "SELECT * FROM devices WHERE id=:id"
+                device = await self.query( statement, {"id" : dev_fields.device_id})
+                device = device[0]
+                single_result["fields"].append({
+                        "device_name" : device.name,
+                        "id" : device.device_key,
+                        "name" : dev_fields.field_name
+                    })
+            response.append(single_result)
+        return response
+
+    async def create_new_dashboard( self, user_id, args ):
+        response = dict()
+
+        device_fields = list()
+        for I in args["fields"]:
+            device = None
+            if I["device_name"]:
+                statement = "SELECT * from devices WHERE name=:name"
+                device = await self.query(statement, {"name": I["device_name"]})
+            elif I["device_id"]:
+                statement = "SELECT * from devices WHERE device_key=:device_key"
+                device = await self.query(statement, {"device_key": I["device_id"]})
+            statement = "SELECT * from device_fields WHERE device_id=:device_id\
+                    AND field_name=:name"
+            field = await self.query(statement, {
+                    "device_id" : device[0].id,
+                    "name" : I["name"]
+                })
+            device_fields.append((device[0], field[0]))
+
+        statement = "INSERT INTO dashboards (user_id, samples) VALUES\
+                (:user_id, :samples) RETURNING ID"
+        dashboard = await self.query(statement, {
+                "user_id" : user_id,
+                "samples" : args["samples"]
+            })
+        dashboard = dashboard[0]
+
+        statement = "INSERT INTO dashboard_fields (dashboard_id, device_fields_id)\
+                VALUES (:dash_id, :field_id)"
+
+        for I in device_fields:
+            await self.query(statement, {
+                    "dash_id" : dashboard.id,
+                    "field_id" : I[1].id
+                })
+
+        response["id"] = dashboard.id
+        response["samples"] = args["samples"]
+        response["fields"] = list()
+        for I in device_fields:
+            single_result = dict()
+            single_result["name"] = I[1].field_name
+            single_result["id"] = I[0].device_key
+            response["fields"].append(single_result)
+
+        await self.application.db.commit()
+
+        self.set_header("Content-Type", "application/json")
+
+        self.write(json.dumps(response))
+
+    async def get_single_dashboard( self, dashboard_id ):
+        response = dict()
+
+        statement = "SELECT * FROM dashboards WHERE id=:id"
+        dashboard = await self.query(statement, {"id": dashboard_id})
+        dashboard = dashboard[0]
+
+        response["id"] = dashboard.id
+        response["samples"] = dashboard.samples
+        response["fields"] = list()
+
+        statement = "SELECT * FROM dashboard_fields WHERE dashboard_id\
+                =:id"
+        fields = await self.query(statement, {"id" : dashboard.id})
+        for I in fields:
+            statement = "SELECT * FROM device_fields WHERE id=:id"
+            dev_field = await self.query(statement, {"id": I.device_fields_id})
+            dev_field = dev_field[0]
+            statement = "SELECT * FROM devices WHERE id=:id"
+            device = await self.query(statement, {"id" : dev_field.device_id})
+            device = device[0]
+            single_result = dict()
+            single_result["name"] = dev_field.field_name
+            single_result["id"] = device.device_key
+            response["fields"].append(single_result)
+
+        return response
+
+    async def delete_single_dashboard( self, dashboard_id ):
+        response = await self.get_single_dashboard( dashboard_id )
+
+        statement = "DELETE FROM dashboard_fields WHERE dashboard_id=:id"
+        await self.query(statement, {"id": dashboard_id})
+
+        statement = "DELETE FROM dashboards WHERE id=:id"
+        await self.query(statement, {"id": dashboard_id})
+
+        await self.application.db.commit()
+
+        return response
+
 
 class RestDeviceHandler(RestBaseHandler):
     async def get(self):
@@ -232,36 +456,6 @@ class RestDeviceSingleHandler(RestBaseHandler):
 
         self.write(json.dumps(device))
 
-    async def patch(self, category, identification):
-        user_id = self.authenticate()
-
-        if not self.request.headers["Content-Type"] == 'application/json':
-            raise tornado.web.HTTPError(400)
-
-        args = tornado.escape.json_decode(self.request.body)
-
-        self.validate_string(identification)
-        device_id = identification
-        device = None
-        if category == 'id':
-            device = await self.get_device_with_id( user_id, device_id )
-        elif category == 'name':
-            device = await self.get_device_with_name( user_id, device_id )
-
-        if device == None:
-            raise tornado.web.HTTPError(404)
-
-        if args["name"]:
-            device["name"] = args["name"]
-        if args["fields"]:
-            device["fields"] = args["fields"]
-
-        await self.update_device( user_id, device )
-
-        self.set_header("Content-Type", "application/json")
-
-        self.write(json.dumps(device))
-
     async def delete(self, category, identification):
         user_id = self.authenticate()
 
@@ -277,42 +471,131 @@ class RestDeviceSingleHandler(RestBaseHandler):
 
         self.write(json.dumps(device))
 
-class RestDataHandler(RestBaseHandler):
-    async def get(self):
+class RestDataSingleHandler(RestBaseHandler):
+    async def get(self, category, identification):
         user_id = self.authenticate()
 
         limit = self.parse_int_argument('limit', 100, 0, 1000)
         page = self.parse_int_argument('page', 0, 0, 0)
         ascending = self.parse_bool_argument('ascending', False)
 
-        samples = await self.get_samples_from_user( user_id,
-                                                    limit,
-                                                    page,
-                                                    ascending )
+        self.validate_string(identification)
+        device_id = identification
+        device = None
+        if category == 'id':
+            device = await self.get_device_with_id( user_id, device_id )
+        elif category == 'name':
+            device = await self.get_device_with_name( user_id, device_id )
 
         self.set_header("Content-Type", "application/json")
 
+        if device == None:
+            raise tornado.web.HTTPError(404)
+
+        samples = await self.get_samples( device["id"],
+                                          limit,
+                                          page,
+                                          ascending )
+
         self.write(json.dumps(samples))
 
-class RestDataSingleHandler(RestBaseHandler):
-    async def get(self):
-        pass
-
-    async def delete(self):
-        pass
 
 class RestDataSingleFieldHandler(RestBaseHandler):
-    async def get(self):
-        pass
+    async def get(self, category, identification):
+        user_id = self.authenticate()
 
-    async def patch(self):
-        pass
+        limit = self.parse_int_argument('limit', 100, 0, 1000)
+        page = self.parse_int_argument('page', 0, 0, 0)
+        ascending = self.parse_bool_argument('ascending', False)
+
+        fields = self.get_arguments('field')
+
+        self.validating_string(identification)
+        device_id = identification
+        device = None
+        if category == 'id':
+            device = await self.get_device_with_id( user_id, device_id )
+        elif category == 'name':
+            device = await self.get_device_with_name( user_id, device_id )
+
+        self.set_header("Content-Type", "application/json")
+
+        if device == None:
+            raise tornado.web.HTTPError(404)
+
+        samples = await self.get_samples( device["id"],
+                                          limit,
+                                          page,
+                                          ascending )
+
+        response = dict()
+        response["total_pages"] = samples["total_pages"]
+        response["page"] = samples["page"]
+
+        response["data"] = dict()
+        for I in fields:
+            if samples["data"][I]:
+                response["data"][I] = samples["data"][I]
+
+        return response
+
+class RestDashboardHandler(RestBaseHandler):
+    async def get(self):
+        user_id = self.authenticate()
+
+        dashboards = await self.get_dashboards_with_user( user_id )
+
+        self.set_header("Content-Type", "application/json")
+
+        self.write(json.dumps(dashboards))
+
+    async def post(self):
+        user_id = self.authenticate()
+        
+        if not self.request.headers["Content-Type"] == 'application/json':
+            raise tornado.web.HTTPError(400)
+
+        print(self.request.body)
+        args = tornado.escape.json_decode(self.request.body)
+
+        if not args["samples"]:
+            raise tornado.web.HTTPError(400)
+        if not args["fields"]:
+            raise tornado.web.HTTPError(400)
+        if len(args["fields"]) == 0:
+            raise HTTPError(400)
+
+        dashboard = await self.create_new_dashboard(user_id, args)
+
+        self.set_header("Content-Type", "application/json")
+
+        self.write(json.dumps(dashboard))
+
+class RestDashboardSingleHandler(RestBaseHandler):
+    async def get(self, dashboard_id):
+        user_id = self.authenticate()
+
+        dashboard = await self.get_single_dashboard( dashboard_id )
+
+        self.set_header("Content-Type", "application/json")
+
+        self.write(json.dumps(dashboard))
+
+    async def delete(self, dashboard_id):
+        user_id = self.authenticate()
+
+        dashboard = await self.delete_single_dashboard( dashboard_id )
+
+        self.set_header("Content-Type", "application/json")
+
+        self.write(json.dumps(dashboard))
 
 def handlers():
     return [
         (r"/api/device", RestDeviceHandler),
         (r"/api/device/by_(id|name)/(.*?)", RestDeviceSingleHandler),
-        (r"/api/data", RestDataHandler),
         (r"/api/data/by_(id|name)/(.*?)", RestDataSingleHandler),
-        (r"/api/data/by_(id|name)/(.*?)/field", RestDataSingleFieldHandler)
+        #(r"/api/data/by_(id|name)/(.*?)/field", RestDataSingleFieldHandler),
+        (r"/api/dashboard", RestDashboardHandler),
+        (r"/api/dashboard/(.*?)", RestDashboardSingleHandler),
     ]
