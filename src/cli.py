0ae2e2fe4e4e9640dc778f9eae6a62c5c962165a
Folkert Kevelam <folkert@pyllr.nl>
2022-03-17 17:22:29 +0100

    Add direct injection of test data

--- src/cli.py
+++ src/cli.py
@@ -4,10 +4,15 @@ import argparse
 import datetime
 import sqlite3
 import string
+import time
+import random
+import msgpack
 
 parser = argparse.ArgumentParser(description='Client for database')
 parser.add_argument('--key', help='generate new login key', action="store_true")
+parser.add_argument('--data', help='generate random data', action="store_true")
 
+random.seed(time.time())
 args = parser.parse_args()
 
 if args.key:
@@ -23,3 +28,21 @@ if args.key:
     db.close()
     print(secret_key)
 
+if args.data:
+    db = sqlite3.connect("dashboard.db")
+    cur = db.cursor()
+
+    for I in range(0, 1000):
+        timestamp = time.time_ns()
+        device_id = 1
+        data = msgpack.packb([random.randint(-1000, 1000)])
+        cur.execute("INSERT INTO samples( device_id, timestamp, data ) \
+                VALUES (:device_id, :timestamp, :data)", {
+                        "device_id" : device_id,
+                        "timestamp" : timestamp,
+                        "data" : data
+                    })
+
+
+    db.commit()
+    db.close()
